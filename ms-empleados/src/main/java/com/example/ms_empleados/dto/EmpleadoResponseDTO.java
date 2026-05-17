467465b7108d6dd2b56d2e81f9856b899db8dfa1
Christian Montolla <ch.montolla@duocuc.cl>
2026-05-17 19:17:06 -0400

    christian: RequestDTO

---
+++ ms-empleados/src/main/java/com/example/ms_empleados/dto/EmpleadoResponseDTO.java
@@ -0,0 +1,5 @@
+package com.example.ms_empleados.dto;
+
+public class EmpleadoResponseDTO {
+
+}
--- ms-empleados/src/main/java/com/example/ms_empleados/repository/EmpleadoRepository.java
+++ ms-empleados/src/main/java/com/example/ms_empleados/repository/EmpleadoRepository.java
@@ -4,5 +4,5 @@ import com.example.ms_empleados.entity.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
-    
+
 }
