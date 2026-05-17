10f2c2573c2b71dd943332aae2deffd05bb7ad91
Christian Montolla <ch.montolla@duocuc.cl>
2026-05-16 20:31:47 -0400

    christian: aca terminamos la clase del cliente

--- ms-clientes/src/main/java/com/example/ms_clientes/entity/Cliente.java
+++ ms-clientes/src/main/java/com/example/ms_clientes/entity/Cliente.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "clientes")
 @Data
@@ -15,5 +18,21 @@ import lombok.NoArgsConstructor;
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)// hace que sea auto incrementable el ID
+    private Integer id;
     
+    //---5 atributos
+    @Column(nullable = false, unique = true, length = 10)
+    private String rut;
+
+    @Column (nullable = false)
+    private String nombreCompleto;
+
+    @Column(nullable = false)
+    private BigDecimal ingresoMensual;
+
+    @Column(nullable = false)
+    private Boolean activo;
+
+    @Column(nullable = false)
+    private LocalDate fechaNacimiento;
 }
