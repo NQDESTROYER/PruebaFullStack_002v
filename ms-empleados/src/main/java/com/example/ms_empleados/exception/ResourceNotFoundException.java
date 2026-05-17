9c0eb6cb9f9102e640eb145257bde09edea7ac2c
Christian Montolla <ch.montolla@duocuc.cl>
2026-05-17 18:35:53 -0400

    christian: listo con excepciones

--- ms-empleados/src/main/java/com/example/ms_empleados/exception/ResourceNotFoundException.java
+++ ms-empleados/src/main/java/com/example/ms_empleados/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.example.ms_empleados.repository;
+package com.example.ms_empleados.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
