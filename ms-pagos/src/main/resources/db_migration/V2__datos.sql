211ce70e91795acb2d6cd6084978c8785153847b
NQDESTROYER <youngbarbersss@gmail.com>
2026-06-21 23:05:02 -0400

    fix(config): corregir migraciones de BD y sincronizar conexiones a Eureka

--- ms-pagos/src/main/resources/db_migration/V2__datos.sql
+++ ms-pagos/src/main/resources/db_migration/V2__datos.sql
@@ -1,4 +1,5 @@
 -- V2__datos.sql
+-- V2__datos.sql
 INSERT INTO pagos (monto, fecha_pago, metodo_pago, reserva_id)
 VALUES (500.00, '2026-07-11 10:00:00', 'TARJETA', 1),
        (300.00, '2026-08-06 11:00:00', 'TRANSFERENCIA', 2),
