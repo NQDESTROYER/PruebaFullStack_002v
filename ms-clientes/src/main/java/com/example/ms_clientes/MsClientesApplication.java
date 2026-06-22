61f0876439fa3d339da39484bd5ceced4e77be67
Christian Montolla <ch.montolla@duocuc.cl>
2026-06-21 20:29:37 -0400

    Integracion Eureka y ajustes de configuracion

--- ms-clientes/src/main/java/com/example/ms_clientes/MsClientesApplication.java
+++ ms-clientes/src/main/java/com/example/ms_clientes/MsClientesApplication.java
@@ -2,8 +2,10 @@ package com.example.ms_clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.example.ms_clientes")
+@EnableDiscoveryClient
 public class MsClientesApplication {
 
 	public static void main(String[] args) {
